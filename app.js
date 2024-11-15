1fc80d93bb4720d086220598e5ff197771ddcadb
Nazbibi97 <nazbibi103@gmail.com>
2024-11-15 19:10:03 +0500

    ATM machine Proccess

---
+++ app.js
@@ -0,0 +1,24 @@
+ 
+
+function contiProccess(){
+    document.getElementById("atm").style.display= "none"
+    document.getElementById("enterAmout").style.display= "block"
+    document.getElementById("but").style.display= "none"
+
+
+    
+}
+function contiProccessTo(){
+    document.getElementById("atm").style.display= "none"
+    document.getElementById("enterAmout").style.display= "none"
+    document.getElementById("but").style.display= "block"
+
+
+    
+}
+// function "withdraw(){
+
+// }
+
+
+
